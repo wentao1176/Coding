6592f331ab07ae60ab7c4ad9aed820d812f34ffe
XWT <2918348982@qq.com>
2026-03-27 12:40:10 +0800

    我的世界

---
+++ minecraft-game/start-server.ps1
@@ -0,0 +1,28 @@
+$listener = New-Object System.Net.HttpListener
+$listener.Prefixes.Add('http://localhost:9000/')
+$listener.Start()
+Write-Host 'Server started at http://localhost:9000'
+
+while ($listener.IsListening) {
+    $context = $listener.GetContext()
+    $response = $context.Response
+    $path = $context.Request.Url.LocalPath
+    
+    if ($path -eq '/') {
+        $path = '/fixed-index.html'
+    }
+    
+    $filePath = Join-Path (Get-Location) $path.TrimStart('/')
+    
+    if (Test-Path $filePath -PathType Leaf) {
+        $content = Get-Content $filePath -Raw
+        $response.ContentType = 'text/html'
+        $buffer = [System.Text.Encoding]::UTF8.GetBytes($content)
+        $response.ContentLength64 = $buffer.Length
+        $response.OutputStream.Write($buffer, 0, $buffer.Length)
+    } else {
+        $response.StatusCode = 404
+    }
+    
+    $response.Close()
+}
